3181d6876b202fcc94839a8795d90c9d7b3d355c
mintycube <90507714+mintycube@users.noreply.github.com>
2024-07-10 11:32:52 +0500

    add transparency and change font size
    This takes effect only when using st as a scratchpad in dwm.
    Otherwise transparency value from xresources is used.
    Bug or a Feature??

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=13:antialias=true:autohint=true";
 #if FONT2_PATCH
 /* Spare fonts */
 static char *font2[] = {
@@ -166,7 +166,7 @@ unsigned int tabspaces = 8;
 
 #if ALPHA_PATCH
 /* bg opacity */
-float alpha = 1.0;
+float alpha = 0.50;
 #if ALPHA_GRADIENT_PATCH
 float grad_alpha = 0.54; //alpha value that'll change
 float stat_alpha = 0.46; //constant alpha value that'll get added to grad_alpha
